f3f9bde312b68aa35c4cf35755b2cc8f764df314
to-mrR0w <marci.sch.2006@gmail.com>
2024-10-21 23:08:10 +0200

    IncRename

---
+++ lua/morrow/lazy/inc-rename.lua
@@ -0,0 +1,12 @@
+return {
+	"smjonas/inc-rename.nvim",
+	config = function()
+		require("inc_rename").setup()
+
+		vim.keymap.set("n", "<leader>cr", function()
+			return ":IncRename " .. vim.fn.expand("<cword>")
+		end, { expr = true })
+	end
+
+
+}
